5d404560ab67d8de40dcacea62e021ad49403fac
최성준 <csj2702@naver.com>
2020-10-07 23:25:46 +0900

    init commit

---
+++ src/containers/main/home/post/PostComments.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {TouchableOpacity, Text} from 'react-native';
+import colors from 'res/colors';
+
+export default function PostComments({post}) {
+  return (
+    <TouchableOpacity
+      style={{marginTop: 5, marginStart: 15}}
+      onPress={() => console.log('Pressed Post Comments')}>
+      <Text style={{color: colors.textFaded2}}>
+        View all {post.commentCount} comments
+        댓글리스트 
+      </Text>
+    </TouchableOpacity>
+  );
+}
